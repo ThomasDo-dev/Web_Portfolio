4e4e70bacba2c0759cd03bbcf94d77f5d07a6ecd
chickwn <chickwn@gmail.com>
2025-01-09 03:13:53 +0000

    initial buttons which redirects to different urls initial base html with home,work,about,play pages

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template #this function render template is used to render HTML templates
 
-app = Flask(__name__)
+app = Flask(__name__) #Create Flask instance and name is special variable helps with locating the resources like templates and static files
 
-@app.route("/")
-
+@app.route("/") #Tells Flask which Url should trigger the function below which in this case is the root which is https://127.0.0.1:5000/
 def home():
-    return "Welcome to my personal website"
+    return render_template("home.html")
 
-if __name__ == "__main__":
-    app.run(debug = True)
+@app.route("/work")
+def work():
+    return render_template("work.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/play")
+def play():
+    return render_template("play.html")
+
+
+if __name__ == "__main__": #__name__ a built-in Python variable holds the name of the current module and this if statement ensure that this part of the code only runs when the script is executed directly and not when it's imported
+    app.run(debug = True)  # this if and app.run ensures Flask Development server is only started when running this file and not other files that might import this file
