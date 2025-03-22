ca03949e0b6b9aee602190c6f8a2af21ed8fb7fe
ThomasDo-dev <thomas.tungdoviet@gmail.com>
2025-03-22 17:10:51 +0000

    - added css for styling instead of embedded it in the html code - added a profile pic - made home and base to default the same url

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template #this function render template is used to render HTML templates
+from flask import Flask, render_template
 
-app = Flask(__name__) #Create Flask instance and name is special variable helps with locating the resources like templates and static files
+app = Flask(__name__)
 
-@app.route("/") #Tells Flask which Url should trigger the function below which in this case is the root which is https://127.0.0.1:5000/
+@app.route("/")
 def home():
     return render_template("home.html")
 
@@ -18,8 +18,5 @@ def about():
 def play():
     return render_template("play.html")
 
-#asdasda
-
-
-if __name__ == "__main__": #__name__ a built-in Python variable holds the name of the current module and this if statement ensure that this part of the code only runs when the script is executed directly and not when it's imported
-    app.run(debug = True)  # this if and app.run ensures Flask Development server is only started when running this file and not other files that might import this file
+if __name__ == "__main__":
+    app.run(debug=True)
